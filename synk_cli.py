0b2a56a325b3aa5ae129c796443e8478ff11a7d8
fa11en <fa11en@localhost.localdomain>
2017-04-09 13:12:15 -0400

    test

---
+++ synk_cli.py
@@ -0,0 +1,18 @@
+import socket
+import os
+import time
+import multiprocessing
+
+git_server = "https://github.com/Eternali/synk.git"
+
+
+def upload_changes():
+    os.system("git add -f . && git commit -am 'autocommit'")
+    
+
+
+
+while True:
+    os.system("git add -f . && git commit -am 'autocommit'")
+
+
