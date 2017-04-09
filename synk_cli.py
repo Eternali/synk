1af0f334d7839d831308e94b3158902cdc53b0fe
fa11en <fa11en@localhost.localdomain>
2017-04-09 13:20:53 -0400

    test

--- synk_cli.py
+++ synk_cli.py
@@ -1,14 +1,22 @@
-import socket
-import os
-import time
-import multiprocessing
+import socket, os, time, multiprocessing, pexpect
 
 git_server = "https://github.com/Eternali/synk.git"
+usernm = "Eternali"
+passwd = ""
 
 
 def upload_changes():
     os.system("git add -f . && git commit -am 'autocommit'")
-    
+    child = pexpect.spawn("git push origin master")
+    child.expect(["Username for 'https://github.com':"])
+    child.sendline(usernm)
+    child.expect(["Password for 'https://%s@github.com':" % usernm])
+    child.sendline(passwd)
+    child.close()
+
+
+def get_changes():
+    os.system("git pull origin master")
 
 
 
